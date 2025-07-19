927a9d5f2ec264b854ed753a89f37ba924435dc4
Aninik4444 <166229209+Aninik4444@users.noreply.github.com>
2025-07-19 22:01:51 +0530

    Update employerApi.js
    render url for backend

--- src/api/employerApi.js
+++ src/api/employerApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL = 'https://justskillpay-backend-b7lg.onrender.com';
 
 const api = axios.create({
   baseURL: BASE_URL,
